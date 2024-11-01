2eeaa9fb56c5f1702eba244711da63c92cb89033
anouar <anwar.class36flow@gmail.com>
2024-11-01 16:06:33 +0100

    classrom test

--- invoice.js
+++ invoice.js
@@ -31,3 +31,33 @@ document.addEventListener('DOMContentLoaded', function() {
     // If you finish the above functions, consider adding a delete button
  
 });
+
+
+
+var nuber1 = document.getElementById("nub1").value;
+var nuber2 = document.getElementById("nub2").value;
+var solition1 = nuber1 * nuber2;
+document.getElementById("solition1").innerHTML.value = solition1 ;
+console.log(test)
+
+
+var nuber3 = document.getElementById("nub3").value;
+var nuber4 = document.getElementById("nub4").value;
+var solition1 = nuber1 * nuber2;
+document.getElementById("solition2").value = solition2;
+
+var nuber5 = document.getElementById("nub5").value;
+var nuber6 = document.getElementById("nub6").value;
+var solition1 = nuber1 * nuber2;
+document.getElementById("solition3").value = solition3;
+
+
+
+
+// function Calculate()
+// {
+// var resources = document.getElementById('a1').value;
+// var minutes = document.getElementById('a2').value; 
+// document.getElementById('a3').value=parseInt(resources) *       parseInt(minutes);
+//  document.form1.submit();
+//    }
